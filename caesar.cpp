3f32a3fa369851c811beada7b6f7435c1f8ca430
sonu1448 <damoliaarvind@gmail.com>
2020-01-21 01:42:31 +0530

    Update caesar.cpp

--- caesar.cpp
+++ caesar.cpp
@@ -1,8 +1,9 @@
+// it is monoalphabetic substitution cipher
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
   string cipher="PHHW PH DIWHU WKH WRJD SDUWB"; // cipher text to decript
-  for(int i=0;i<26;i++){                        // less secure bcoz it has only 26 possibility
+  for(int i=0;i<26;i++){                        // less secure bcoz it has only 25 possibility
       for(int j=0;j<cipher.length();j++){
           if(cipher[j]!=' '){
               int temp=cipher[j]-'A';
