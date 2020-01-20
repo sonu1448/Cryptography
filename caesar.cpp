1655cbea0890c0a30b6f40e94bdb3fcb772f7d85
sonu1448 <damoliaarvind@gmail.com>
2020-01-21 01:38:47 +0530

    Create caesar.cpp

---
+++ caesar.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string cipher="PHHW PH DIWHU WKH WRJD SDUWB"; // cipher text to decript
+  for(int i=0;i<26;i++){                        // less secure bcoz it has only 26 possibility
+      for(int j=0;j<cipher.length();j++){
+          if(cipher[j]!=' '){
+              int temp=cipher[j]-'A';
+              cout<<char((temp+i)%26+'A');
+            }
+          else{
+              cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
